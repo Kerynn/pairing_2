f6d12d8001055a1a53db4d0b01ea27143c487427
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-07 18:37:44 -0700

    Added friend2 variable

--- friends.rb
+++ friends.rb
@@ -1,7 +1,10 @@
-friend = "Marco"
+friend = "Beth"
+friend2 = "David"
 
 if friend == "Marco"
 p "Marco is my best friend!"
+elsif friend2 == "David"
+  p "David is also a very good friend."
 else
   p "I don't need friends because I have my dog."
 end
